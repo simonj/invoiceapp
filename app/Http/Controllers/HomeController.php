b574ae148012ffc8ea5939dbbdea2c2a4db22bfc
Simon <sj@obviux.dk>
2017-05-21 16:22:22 +0200

    Added to route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,17 +6,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-
-        // $this->middleware('subscribed');
-    }
 
     /**
      * Show the application dashboard.

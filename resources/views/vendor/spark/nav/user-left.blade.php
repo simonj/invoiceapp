7ba19f5a2e05a177b43b2619edeba13c3253c90a
Simon <sj@obviux.dk>
2017-05-21 16:52:32 +0200

    Add icons

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -1,3 +1,4 @@
 <!-- Left Side Of Navbar -->
-<li><a href="{{ url('clients') }}">Clients</a></li>
-<li><a href="{{ url('invoices') }}">Invoices</a></li>
+<li><a href="{{ url('home') }}"><i class="fa fa-bar-chart"></i> Dashboard</a></li>
+<li><a href="{{ url('clients') }}"><i class="fa fa-users"></i> Clients</a></li>
+<li><a href="{{ url('invoices') }}"><i class="fa fa-files-o"></i> Invoices</a></li>

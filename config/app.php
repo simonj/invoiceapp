71a56ff47a7409af92c5e59e8835d1e293ea20cb
Simon <sj@obviux.dk>
2017-05-21 16:22:42 +0200

    Add pdf generator

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
+        Barryvdh\Snappy\ServiceProvider::class,
+
 
 
         /*
@@ -230,6 +232,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'PDF' => Barryvdh\Snappy\Facades\SnappyPdf::class,
+        'SnappyImage' => Barryvdh\Snappy\Facades\SnappyImage::class,
 
     ],
 

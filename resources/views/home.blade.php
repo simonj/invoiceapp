554386b6dd1910a5c9037991147853ba8a20a147
Simon <sj@obviux.dk>
2017-05-25 14:08:07 +0200

    Add more dashboard markup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,39 @@
     <div class="container">
         <!-- Application Dashboard -->
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+
+            <div class="col-md-4">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">All time</div>
 
                     <div class="panel-body">
-                        Your application's dashboard.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                        <h1>$45,321</h1>
+                    </div><!-- panel-body -->
+                </div><!-- panel -->
+            </div><!-- /.col-md-3 -->
+
+            <div class="col-md-4">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">This week</div>
+
+                    <div class="panel-body">
+                        <h1>$1,500</h1>
+                    </div><!-- panel-body -->
+                </div><!-- panel -->
+            </div><!-- /.col-md-3 -->
+
+
+            <div class="col-md-4">
+                <div class="panel panel-default text-center">
+                    <div class="panel-heading">Today</div>
+
+                    <div class="panel-body">
+                        <h1>$449</h1>
+                    </div><!-- panel-body -->
+                </div><!-- panel -->
+            </div><!-- /.col-md-3 -->
+
+        </div><!-- row -->
+    </div><!-- container -->
 </home>
 @endsection

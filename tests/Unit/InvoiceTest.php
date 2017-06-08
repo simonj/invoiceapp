f7e6ba9840dbbaa9531b91ae18a553be46c03d90
Simon <sj@obviux.dk>
2017-06-08 08:46:34 +0200

    WIP test invoice controller

---
+++ tests/Unit/InvoiceTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Client;
+use App\ClientInvoice;
+use App\ClientInvoiceItem;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class InvoiceTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_user_can_create_invoice()
+    {
+        $user = factory(User::class)->create();
+
+        $client = factory(Client::class)->create()->toArray();
+
+        $invoice = factory(ClientInvoice::class)->create([
+            'user_id'   => $user->id,
+            'client_id' => $client['id'],
+        ])->toArray();
+
+        $invoiceItems = factory(ClientInvoiceItem::class)->create([
+            'client_invoice_id' => $invoice['id'],
+        ]);
+
+        $params = [
+            'invoice' => $invoice,
+            'client'  => $client,
+            'items'   => $invoiceItems,
+            'date' => Carbon::now(),
+            'amount' => '100'
+        ];
+
+        $response = $this->actingAs($user)
+            ->call('POST', '/invoices', $params);
+
+        $response->assertStatus(200);
+    }
+}

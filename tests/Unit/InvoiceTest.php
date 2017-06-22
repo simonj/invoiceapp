eaccffc5926af7694015607a7e773b86b204b64a
Simon <sj@obviux.dk>
2017-06-22 08:44:56 +0200

    Fix test for user can create invoice

--- tests/Unit/InvoiceTest.php
+++ tests/Unit/InvoiceTest.php
@@ -14,6 +14,7 @@ use Tests\TestCase;
 class InvoiceTest extends TestCase
 {
     use DatabaseMigrations;
+    use DatabaseTransactions;
 
     /**
      * A basic test example.
@@ -24,16 +25,18 @@ class InvoiceTest extends TestCase
     {
         $user = factory(User::class)->create();
 
-        $client = factory(Client::class)->create()->toArray();
+        $client = factory(Client::class)->create([
+            'user_id' => $user->id
+        ])->toArray();
 
         $invoice = factory(ClientInvoice::class)->create([
             'user_id'   => $user->id,
             'client_id' => $client['id'],
         ])->toArray();
 
-        $invoiceItems = factory(ClientInvoiceItem::class)->create([
+        $invoiceItems = factory(ClientInvoiceItem::class, 2)->create([
             'client_invoice_id' => $invoice['id'],
-        ]);
+        ])->toArray();
 
         $params = [
             'invoice' => $invoice,

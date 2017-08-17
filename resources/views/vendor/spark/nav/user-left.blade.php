0a3b8231a783d90aa79b0328c9fc4fcb709f3c15
Simon <sj@obviux.dk>
2017-08-17 10:42:53 +0200

    WIP landing page

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -1,4 +1,73 @@
 <!-- Left Side Of Navbar -->
-<li><a href="{{ url('home') }}"><i class="fa fa-bar-chart"></i> Dashboard</a></li>
-<li><a href="{{ url('clients') }}"><i class="fa fa-users"></i> Clients</a></li>
-<li><a href="{{ url('invoices') }}"><i class="fa fa-files-o"></i> Invoices</a></li>
+
+<!-- Dashboard -->
+<li>
+    <a href="{{ url('home') }}">
+        <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="web-UI" transform="translate(-168.000000, -56.000000)" fill-rule="nonzero" fill="#6B6E74">
+                    <g id="menu">
+                        <g transform="translate(0.000000, 31.000000)">
+                            <g id="menu-items" transform="translate(0.000000, 4.000000)">
+                                <g id="item" transform="translate(168.000000, 21.000000)">
+                                    <g class="icon">
+                                        <path d="M10,20 C4.4771525,20 6.76353751e-16,15.5228475 0,10 C-6.76353751e-16,4.4771525 4.4771525,1.01453063e-15 10,0 C15.5228475,3.38176876e-16 20,4.4771525 20,10 C20,15.5228475 15.5228475,20 10,20 Z M4.4,15.71 C7.77888937,13.4093414 12.2211106,13.4093414 15.6,15.71 C17.9211533,13.4348147 18.6350439,9.98142057 17.4062522,6.97238556 C16.1774604,3.96335056 13.2502647,1.99685726 10,1.99685726 C6.74973528,1.99685726 3.82253955,3.96335056 2.59374785,6.97238556 C1.36495614,9.98142057 2.07884672,13.4348147 4.4,15.71 Z M10.52,8.07 L13.54,5.05 L14.95,6.46 L11.93,9.48 C12.1659554,10.3545801 11.7846297,11.2785498 11.0005298,11.7321493 C10.2164299,12.1857488 9.2253607,12.0557054 8.58482763,11.4151724 C7.94429456,10.7746393 7.81425118,9.78357013 8.26785069,8.9994702 C8.72145019,8.21537026 9.64541995,7.83404459 10.52,8.07 L10.52,8.07 Z" id="Shape"></path>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+        Dashboard
+    </a>
+</li>
+
+<!-- Clients -->
+<li>
+    <a href="{{ url('clients') }}">
+        <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="web-UI" transform="translate(-305.000000, -57.000000)" fill-rule="nonzero" fill="#6B6E74">
+                    <g id="menu">
+                        <g transform="translate(0.000000, 31.000000)">
+                            <g id="menu-items" transform="translate(0.000000, 4.000000)">
+                                <g id="item" transform="translate(305.000000, 21.000000)">
+                                    <g class="icon" transform="translate(0.000000, 1.000000)">
+                                        <path d="M7,8 C4.790861,8 3,6.209139 3,4 C3,1.790861 4.790861,3.66080864e-16 7,-3.97360096e-17 C9.209139,9.55453165e-17 11,1.790861 11,4 C11,6.209139 9.209139,8 7,8 L7,8 Z M7,9 C9.15,9 11.2,9.4 13.1,10.09 L12,16 L10.75,16 L10,20 L4,20 L3.25,16 L2,16 L0.9,10.09 C2.85466365,9.37539672 4.91881648,9.00655629 7,9 L7,9 Z M15.31,9.17 C16.63,9.35 17.9,9.65 19.11,10.09 L18,16 L16.75,16 L16,20 L12.04,20 L12.41,18 L13.66,18 L15.31,9.17 L15.31,9.17 Z M13,0 C14.600301,-0.0137201584 16.0547518,0.927723735 16.6975026,2.39333721 C17.3402534,3.85895069 17.0475756,5.56660506 15.9534574,6.73453351 C14.8593392,7.90246196 13.174397,8.30583979 11.67,7.76 C13.450955,5.56966985 13.450955,2.43033015 11.67,0.24 C12.1,0.1 12.53,0 13,0 Z" id="Shape"></path>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+        Clients
+    </a>
+</li>
+
+<!-- Invoices -->
+<li>
+    <a href="{{ url('invoices') }}">
+        <svg width="20px" height="18px" viewBox="0 0 20 18" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="web-UI" transform="translate(-414.000000, -58.000000)" fill-rule="nonzero" fill="#6B6E74">
+                    <g id="menu">
+                        <g transform="translate(0.000000, 31.000000)">
+                            <g id="menu-items" transform="translate(0.000000, 4.000000)">
+                                <g id="item" transform="translate(414.000000, 21.000000)">
+                                    <g class="icon" transform="translate(0.000000, 2.000000)">
+                                        <path d="M10,0 L20,6 L10,12 L0,6 L10,0 Z M16.67,10 L20,12 L10,18 L0,12 L3.33,10 L10,14 L16.67,10 Z" id="Shape"></path>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+        Invoices
+    </a>
+</li>
